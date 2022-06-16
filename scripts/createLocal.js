98b464eff268f14521bd660dfb8bc1c2f1d2d25f
Foivos <foivos@umich.edu>
2022-06-16 11:27:53 +0300

    fixed bugs.

--- scripts/createLocal.js
+++ scripts/createLocal.js
@@ -1,17 +1,12 @@
 const { createAndExport, utils: { setJSON, deployContract } } = require('@axelar-network/axelar-local-dev');
 const { Wallet, utils: {keccak256, defaultAbiCoder} } = require('ethers');
 
-const ConstAddressDeployer = require('../build/ConstAddressDeployer.json');
-
 (async () => {
     const deployer_key = keccak256(defaultAbiCoder.encode(['string'], ['this is a random string to get a random account. You need to provide the private key for a funded account here.']));
     const deployer_address = new Wallet(deployer_key).address;
-    const weth_addresses = {};
 
     async function callback(chain, info) {
         await chain.giveToken(deployer_address, 'aUSDC', 100e6);
-        const contract = await deployContract(new Wallet(deployer_key, chain.provider), ConstAddressDeployer);
-        info.constAddressDeployer = contract.address;
     }
 
     const toFund = [deployer_address]
