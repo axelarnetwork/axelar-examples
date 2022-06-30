ad4784357c28edb29bc65e9bdef04bfb27f0c495
Jon Ator <jonathanator0@gmail.com>
2022-06-30 17:42:33 -0500

    Fix solidity imports

--- examples/nonced-execution/NoncedExecutable.sol
+++ examples/nonced-execution/NoncedExecutable.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.9;
 
 import { IAxelarGateway } from '@axelar-network/axelar-cgp-solidity/contracts/interfaces/IAxelarGateway.sol';
 import { IAxelarExecutable } from '@axelar-network/axelar-cgp-solidity/contracts/interfaces/IAxelarExecutable.sol';
-import { StringToAddress } from 'axelar-utils-solidity/src/StringAddressUtils.sol';
+import { StringToAddress } from 'axelar-utils-solidity/contracts/StringAddressUtils.sol';
 
 abstract contract NoncedExecutable is IAxelarExecutable {
     using StringToAddress for string;
