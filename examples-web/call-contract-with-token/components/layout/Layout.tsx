dc2d919341848c48b1c16113f096149151c3c10e
vladwulf <agaev.vladimir@gmail.com>
2022-07-12 19:04:00 +0100

    chore: fix padding

--- examples-web/call-contract-with-token/components/layout/Layout.tsx
+++ examples-web/call-contract-with-token/components/layout/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout: React.FC<LayoutProps> = (props) => {
   return (
     <div className="h-auto min-h-screen">
       <Navbar />
-      <main className="container px-4 pt-20 mx-auto">{props.children}</main>
+      <main className="container px-4 py-20 mx-auto">{props.children}</main>
     </div>
   );
 };
