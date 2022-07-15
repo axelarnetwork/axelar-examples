f2d6047fb202e728686748a45fe1930aca9efd7b
Canh Trinh <canh@axelar.network>
2022-07-15 14:02:50 -0400

    chore: code setup for crosschain swap

---
+++ examples-web/crosschain-swap/contracts/MessageSender.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import {IERC20} from "@axelar-network/axelar-cgp-solidity/contracts/interfaces/IERC20.sol";
+import {IAxelarGateway} from "@axelar-network/axelar-cgp-solidity/contracts/interfaces/IAxelarGateway.sol";
+import {IAxelarGasService} from "@axelar-network/axelar-cgp-solidity/contracts/interfaces/IAxelarGasService.sol";
+
+contract MessageSender {
+    IAxelarGasService gasReceiver;
+    IAxelarGateway gateway;
+
+    constructor(address _gateway, address _gasReceiver) {
+        gateway = IAxelarGateway(_gateway);
+        gasReceiver = IAxelarGasService(_gasReceiver);
+    }
+
+    function sendToMany(
+        string memory destinationChain,
+        string memory destinationAddress,
+        address[] calldata destinationAddresses,
+        string memory symbol,
+        uint256 amount
+    ) external payable {
+        address tokenAddress = gateway.tokenAddresses(symbol);
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
+        IERC20(tokenAddress).approve(address(gateway), amount);
+        bytes memory payload = abi.encode(destinationAddresses);
+        if (msg.value > 0) {
+            gasReceiver.payNativeGasForContractCallWithToken{value: msg.value}(
+                address(this),
+                destinationChain,
+                destinationAddress,
+                payload,
+                symbol,
+                amount,
+                msg.sender
+            );
+        }
+        gateway.callContractWithToken(
+            destinationChain,
+            destinationAddress,
+            payload,
+            symbol,
+            amount
+        );
+    }
+}
