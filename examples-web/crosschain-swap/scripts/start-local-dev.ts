e07fb0f4905ad6adce56108b457f7d97e8a18bd9
Canh Trinh <canh@axelar.network>
2022-07-18 11:30:40 -0400

    chore: pushing up latest

--- examples-web/crosschain-swap/scripts/start-local-dev.ts
+++ examples-web/crosschain-swap/scripts/start-local-dev.ts
@@ -3,19 +3,15 @@ import { Network } from "@axelar-network/axelar-local-dev/dist/Network";
 import { wallet } from "../config/constants";
 
 // deploy network
+async function callback(chain: any, info: any) {
+  await chain.deployToken('Axelar Wrapped USDC', 'aUSDC', 6, BigInt(1e17));
+  console.log("chain in question",chain.name, wallet.address)
+  for (const address of [wallet.address]) await chain.giveToken(address, 'aUSDC', BigInt(1e15));
+  return null;
+}
 createAndExport({
   accountsToFund: [wallet.address],
-  chains: ["Moonbeam", "Avalanche"],
-  chainOutputPath: "config/local.json",
-  async callback(network: Network) {
-    if (network.name === "Avalanche") {
-      await network.giveToken(
-        wallet.address,
-        "aUSDC",
-        BigInt("100000000000000"),
-      );
-    }
-
-    return null;
-  },
+  chains: ["Avalanche", "Moonbeam"],
+  chainOutputPath: "./config/local.json",
+  callback
 });
