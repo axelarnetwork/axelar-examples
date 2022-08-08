d7719a531ad5074474e78d126059d87a4ea57966
Canh Trinh <canh@axelar.network>
2022-08-08 12:00:01 -0400

    feat: initial commit for nft linker

---
+++ examples-web/README.md
@@ -0,0 +1,6 @@
+The `examples-web` section has a collection of end-to-end examples that include:
+
+-   basic smart contracts invoking cross-chain calls
+-   a simple webapp that invokes methods on the source chain smart contract
+
+Each example is standalone with their own instructions to deploy and run the application, both locally and in testnet.
---
+++ examples-web/nft-linker/components/layout/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Navbar = () => {
+    return (
+        <header className="h-16 bg-white shadow-sm">
+            <nav className="container flex items-center w-full h-full px-4 mx-auto">
+                <div>
+                    <img className="h-8" src="https://axelar.network/wp-content/uploads/2022/02/Axelar-Logo-Dark.svg" alt="axelar" />
+                </div>
+            </nav>
+        </header>
+    );
+};
