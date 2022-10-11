d022238fc346cea855151e4a26903d8b4ac19760
Edwin Guajardo <edwin@axelar.network>
2022-10-11 00:38:18 -0500

    feat: Fixed contract functions and added some more logic

--- examples/cross-chain-lending/CompoundInterface.sol
+++ examples/cross-chain-lending/CompoundInterface.sol
@@ -2,54 +2,117 @@
 pragma solidity 0.8.9;
 
 import '@axelar-network/axelar-gmp-sdk-solidity/contracts/executables/AxelarForecallable.sol';
-import './Compound/CErc20Interface.sol';
+import '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol';
+import './interfaces/CErc20Interface.sol';
 
 contract CompoundInterface is AxelarForecallable {
     bytes32 internal constant SELECTOR_SUPPLY_AND_BORROW = keccak256('supplyAndBorrow');
-    bytes32 internal constant SELECTOR_REPAY_BORROW = keccak256('repayBorrow');
+    bytes32 internal constant SELECTOR_REPAY_BORROW = keccak256('repayAndRedeem');
 
-    mapping(address => CErc20Interface) internal _cTokens;
+    mapping(string => CErc20Interface) internal _cTokens;
 
-    constructor(address gateway_) AxelarForecallable(gateway_) {}
+    modifier onlySelf() {
+        require(msg.sender == address(this), 'Function must be called by the same contract only');
+        _;
+    }
+
+    constructor(
+        address gateway_,
+        string[] memory supportedTokens,
+        CErc20Interface[] memory cTokens
+    ) AxelarForecallable(gateway_) {
+        require(supportedTokens.length == cTokens.length, 'Lengths missmatch');
+
+        for (uint256 i; i < supportedTokens.length; i++) {
+            _cTokens[supportedTokens[i]] = cTokens[i];
+        }
+    }
 
     function supplyAndBorrow(
-        CErc20Interface cToken,
+        string calldata sourceChain,
+        string calldata,
+        string calldata supplyTokenSymbol,
         uint256 supplyAmount,
         bytes memory params
-    ) external {
-        uint256 borrowAmount = abi.decode(params, (uint256));
+    ) external onlySelf {
+        (string memory borrowTokenSymbol, uint256 borrowAmount, string memory userAddress) = abi.decode(params, (string, uint256, string));
 
-        _mint(cToken, supplyAmount);
-        _borrow(cToken, borrowAmount);
+        // TODO: keep track of user supplied collateral
+
+        _mint(supplyTokenSymbol, supplyAmount);
+        _borrow(sourceChain, userAddress, borrowTokenSymbol, borrowAmount);
     }
 
-    function repayBorrow(
-        CErc20Interface cToken,
-        uint256 amount,
-        bytes memory
-    ) external {
-        cToken.repayBorrow(amount);
+    function repayAndRedeem(
+        string calldata sourceChain,
+        string calldata,
+        string calldata repayTokenSymbol,
+        uint256 repayAmount,
+        bytes memory params
+    ) external onlySelf {
+        (string memory redeemTokenSymbol, uint256 redeemAmount, string memory userAddress) = abi.decode(params, (string, uint256, string));
+
+        // TODO: validate amounts
+
+        _repayBorrow(repayTokenSymbol, repayAmount);
+        _redeem(sourceChain, userAddress, redeemTokenSymbol, redeemAmount);
     }
 
-    function _mint(CErc20Interface cToken, uint256 amount) internal {
+    function _mint(string calldata tokenSymbol, uint256 amount) internal {
+        CErc20Interface cToken = _cTokens[tokenSymbol];
+        IERC20 tokenAddress = IERC20(gateway.tokenAddresses(tokenSymbol));
+
+        tokenAddress.approve(address(cToken), amount);
         cToken.mint(amount);
     }
 
-    function _borrow(CErc20Interface cToken, uint256 amount) internal {
+    function _borrow(
+        string calldata sourceChain,
+        string memory userAddress,
+        string memory tokenSymbol,
+        uint256 amount
+    ) internal {
+        CErc20Interface cToken = _cTokens[tokenSymbol];
+        IERC20 tokenAddress = IERC20(gateway.tokenAddresses(tokenSymbol));
         cToken.borrow(amount);
+
+        tokenAddress.approve(address(gateway), amount);
+        gateway.sendToken(sourceChain, userAddress, tokenSymbol, amount);
+    }
+
+    function _repayBorrow(string calldata tokenSymbol, uint256 amount) internal {
+        CErc20Interface cToken = _cTokens[tokenSymbol];
+        IERC20 tokenAddress = IERC20(gateway.tokenAddresses(tokenSymbol));
+
+        tokenAddress.approve(address(cToken), amount);
+        cToken.repayBorrow(amount);
+    }
+
+    function _redeem(
+        string calldata sourceChain,
+        string memory userAddress,
+        string memory tokenSymbol,
+        uint256 amount
+    ) internal {
+        CErc20Interface cToken = _cTokens[tokenSymbol];
+        IERC20 tokenAddress = IERC20(gateway.tokenAddresses(tokenSymbol));
+
+        uint256 balanceBefore = tokenAddress.balanceOf(address(this));
+        cToken.redeem(amount);
+        uint256 redeemedAmount = tokenAddress.balanceOf(address(this)) - balanceBefore;
+
+        tokenAddress.approve(address(gateway), redeemedAmount);
+        gateway.sendToken(sourceChain, userAddress, tokenSymbol, redeemedAmount);
     }
 
     function _executeWithToken(
-        string calldata,
-        string calldata,
+        string calldata sourceChain,
+        string calldata sourceAddress,
         bytes calldata payload,
         string calldata tokenSymbol,
         uint256 amount
     ) internal override {
         (bytes memory functionName, bytes memory params) = abi.decode(payload, (bytes, bytes));
-        address tokenAddress = gateway.tokenAddresses(tokenSymbol);
-
-        CErc20Interface cToken = _cTokens[tokenAddress];
 
         bytes4 commandSelector;
         bytes32 functionHash = keccak256(functionName);
@@ -57,13 +120,15 @@ contract CompoundInterface is AxelarForecallable {
         if (functionHash == SELECTOR_SUPPLY_AND_BORROW) {
             commandSelector = CompoundInterface.supplyAndBorrow.selector;
         } else if (functionHash == SELECTOR_REPAY_BORROW) {
-            commandSelector = CompoundInterface.repayBorrow.selector;
+            commandSelector = CompoundInterface.repayAndRedeem.selector;
         } else {
             revert('Invalid function name');
         }
 
         // solhint-disable-next-line avoid-low-level-calls
-        (bool success, ) = address(this).call(abi.encodeWithSelector(commandSelector, cToken, amount, params));
+        (bool success, ) = address(this).call(
+            abi.encodeWithSelector(commandSelector, sourceChain, sourceAddress, tokenSymbol, amount, params)
+        );
 
         // TODO: error handling
     }
