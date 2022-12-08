53f3508072183325450d1d13e0588a62befb6b50
npty <nptytn2@gmail.com>
2022-12-08 20:02:39 +0700

    fix: cross-chain-token example

--- examples/cross-chain-token/index.js
+++ examples/cross-chain-token/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const {
-    getDefaultProvider,
     constants: { AddressZero },
     utils: { defaultAbiCoder },
 } = require('ethers');
@@ -26,35 +25,24 @@ async function deploy(chain, wallet) {
         defaultAbiCoder.encode(['string', 'string'], [name, symbol]),
         'cross-chain-token',
     );
-    chain.crossChainToken = contract.address;
-    console.log(`Deployed ERC20CrossChain for ${chain.name} at ${chain.crossChainToken}.`);
+    chain.crossChainToken = contract;
+    console.log(`Deployed ERC20CrossChain for ${chain.name} at ${chain.crossChainToken.address}.`);
 }
 
 async function test(chains, wallet, options) {
     const args = options.args || [];
     const getGasPrice = options.getGasPrice;
-    for (const chain of chains) {
-        const provider = getDefaultProvider(chain.rpc);
-        chain.wallet = wallet.connect(provider);
-        chain.contract = await deployUpgradable(
-            chain.constAddressDeployer,
-            chain.wallet,
-            ERC20CrossChain,
-            ExampleProxy,
-            [chain.gateway, chain.gasReceiver, decimals],
-            [],
-            defaultAbiCoder.encode(['string', 'string'], [name, symbol]),
-            'cross-chain-token',
-        );
-    }
+
     const source = chains.find((chain) => chain.name === (args[0] || 'Avalanche'));
     const destination = chains.find((chain) => chain.name === (args[1] || 'Fantom'));
+    console.log(destination);
     const amount = parseInt(args[2]) || 1e5;
 
     async function print() {
-        console.log(`Balance at ${source.name} is ${await source.contract.balanceOf(wallet.address)}`);
-        console.log(`Balance at ${destination.name} is ${await destination.contract.balanceOf(wallet.address)}`);
+        console.log(`Balance at ${source.name} is ${await source.crossChainToken.balanceOf(wallet.address)}`);
+        console.log(`Balance at ${destination.name} is ${await destination.crossChainToken.balanceOf(wallet.address)}`);
     }
+
     function sleep(ms) {
         return new Promise((resolve) => {
             setTimeout(() => {
@@ -62,22 +50,25 @@ async function test(chains, wallet, options) {
             }, ms);
         });
     }
-    const initialBalance = (await destination.contract.balanceOf(wallet.address)).toNumber();
+
+    const initialBalance = (await destination.crossChainToken.balanceOf(wallet.address)).toNumber();
     console.log('--- Initially ---');
     await print();
 
     // Set the gasLimit to 3e5 (a safe overestimate) and get the gas price (this is constant and always 1).
     const gasLimit = 3e5;
     const gasPrice = await getGasPrice(source, destination, AddressZero);
-    await (await source.contract.giveMe(amount)).wait();
+    await (await source.crossChainToken.giveMe(amount)).wait();
     console.log('--- After getting some token on the source chain ---');
     await print();
 
     await (
-        await source.contract.transferRemote(destination.name, wallet.address, amount, { value: BigInt(Math.floor(gasLimit * gasPrice)) })
+        await source.crossChainToken.transferRemote(destination.name, wallet.address, amount, {
+            value: BigInt(Math.floor(gasLimit * gasPrice)),
+        })
     ).wait();
 
-    while ((await destination.contract.balanceOf(wallet.address)).toNumber() === initialBalance) {
+    while ((await destination.crossChainToken.balanceOf(wallet.address)).toNumber() === initialBalance) {
         await sleep(2000);
     }
 
