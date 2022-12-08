1e8e5d59d5f304b249c9bf110577ea60b1b32a89
npty <nptytn2@gmail.com>
2022-12-08 19:40:48 +0700

    chore: formatting

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,45 +1,53 @@
 'use strict';
 
-require("dotenv").config();
-const { utils: { setJSON}, testnetInfo } = require('@axelar-network/axelar-local-dev');
-const {  Wallet, getDefaultProvider } = require('ethers');
-const { keccak256, defaultAbiCoder } = require('ethers/lib/utils');
-const { GasCostLogger } = require('./gasCosts');
+require('dotenv').config();
+const {
+    utils: { setJSON },
+    testnetInfo,
+} = require('@axelar-network/axelar-local-dev');
+const { Wallet, getDefaultProvider } = require('ethers');
 
 async function deploy(env, chains, wallet, example) {
-    if(example.preDeploy) {
+    if (example.preDeploy) {
         await example.preDeploy(chains, wallet);
     }
+
     const promises = [];
-    for(const chain of chains) {
+
+    for (const chain of chains) {
         const rpc = chain.rpc;
         const provider = getDefaultProvider(rpc);
         promises.push(example.deploy(chain, wallet.connect(provider)));
     }
+
     await Promise.all(promises);
-    if(example.postDeploy) {
-        for(const chain of chains) {
+
+    if (example.postDeploy) {
+        for (const chain of chains) {
             const rpc = chain.rpc;
             const provider = getDefaultProvider(rpc);
             promises.push(example.postDeploy(chain, chains, wallet.connect(provider)));
         }
+
         await Promise.all(promises);
     }
+
     setJSON(chains, `./info/${env}.json`);
 }
 
 module.exports = {
     deploy,
-}
-
+};
 
 if (require.main === module) {
     const example = require(`../${process.argv[2]}/index.js`);
 
     const env = process.argv[3];
-    if(env == null || (env != 'testnet' && env != 'local')) throw new Error('Need to specify tesntet or local as an argument to this script.');
+    if (env == null || (env !== 'testnet' && env !== 'local'))
+        throw new Error('Need to specify testnet or local as an argument to this script.');
     let temp;
-    if(env == 'local') {
+
+    if (env === 'local') {
         temp = require(`../info/local.json`);
     } else {
         try {
@@ -48,10 +56,11 @@ if (require.main === module) {
             temp = testnetInfo;
         }
     }
+
     const chains = temp;
 
-    const private_key = process.env.EVM_PRIVATE_KEY;
-    const wallet = new Wallet(private_key);
+    const privateKey = process.env.EVM_PRIVATE_KEY;
+    const wallet = new Wallet(privateKey);
 
     deploy(env, chains, wallet, example);
 }
