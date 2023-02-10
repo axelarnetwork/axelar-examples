1da522c8d314ece3258d2f32666ecf0a78f4eaf0
npty <nptytn2@gmail.com>
2023-02-10 22:08:49 +0700

    chore: refactor send-tokens

--- examples-web/src/pages/examples/send-tokens/utils/depositAddressSendToken.ts
+++ examples-web/src/pages/examples/send-tokens/utils/depositAddressSendToken.ts
@@ -1,68 +1,91 @@
-import { ethers, getDefaultProvider, providers } from 'ethers';
-import { AxelarAssetTransfer, Environment } from '@axelar-network/axelarjs-sdk';
-import { IERC20__factory as IERC20, IAxelarGateway__factory as IAxelarGateway } from 'types/contracts/factories/@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces'
-import { isTestnet, wallet } from 'config/constants';
-import { getDepositAddressLocal } from './getDepositAddressLocal';
-import { sleep } from './sleep';
-import { getTransferFee } from './getTransferFee';
+import { ethers } from "ethers";
+import { AxelarAssetTransfer, Environment } from "@axelar-network/axelarjs-sdk";
+import {
+  IERC20__factory as IERC20,
+  IAxelarGateway__factory as IAxelarGateway,
+} from "types/contracts/factories/@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces";
+import {
+  srcChain,
+  srcConnectedWallet,
+  destChain,
+  destConnectedWallet,
+} from "config/constants";
+import { getDepositAddressLocal } from "./getDepositAddressLocal";
+import { sleep } from "./sleep";
+import { getTransferFee } from "./getTransferFee";
 
-const chains = isTestnet
-  ? require("../../../../../config/testnet.json")
-  : require("../../../../../config/chains.json");
-
-const ethereum = chains.find((chain: any) => chain.name === 'Ethereum') as any;
-const avalanche = chains.find((chain: any) => chain.name === 'Avalanche') as any;
-
-if (!ethereum || !avalanche) process.exit(0);
-
-const useMetamask = false; // typeof window === 'object';
-
-const ethereumProvider = useMetamask ? new providers.Web3Provider((window as any).ethereum) : getDefaultProvider(ethereum.rpc);
-const ethereumConnectedWallet = useMetamask ? (ethereumProvider as providers.Web3Provider).getSigner() : wallet.connect(ethereumProvider);
-const avalancheProvider = getDefaultProvider(avalanche.rpc);
-const avalancheConnectedWallet = wallet.connect(avalancheProvider);
-
-const srcGatewayContract = IAxelarGateway.connect(ethereum.gateway, ethereumConnectedWallet);
-const destGatewayContract = IAxelarGateway.connect(avalanche.gateway, avalancheConnectedWallet);
+const srcGatewayContract = IAxelarGateway.connect(
+  srcChain.gateway,
+  srcConnectedWallet
+);
+const destGatewayContract = IAxelarGateway.connect(
+  destChain.gateway,
+  destConnectedWallet
+);
 
 export async function depositAddressSendToken(
-    amount: string,
-    recipientAddress: string,
-    onSent: (data: { txHash: string; depositAddress: string; transferFee: number }) => void,
+  amount: string,
+  recipientAddress: string,
+  onSent: (data: {
+    txHash: string;
+    depositAddress: string;
+    transferFee: number;
+  }) => void
 ) {
-    let depositAddress: string;
+  let depositAddress: string;
 
-    if (process.env.NEXT_PUBLIC_ENVIRONMENT === 'testnet') {
-      const api = new AxelarAssetTransfer({ environment: Environment.TESTNET });
-      depositAddress = await api.getDepositAddress('ethereum', 'avalanche', recipientAddress, 'uausdc');
-    } else {
-      depositAddress = (await getDepositAddressLocal(ethereum.name, avalanche.name, recipientAddress, 'aUSDC', 8500)) as string;
-    }
+  if (process.env.NEXT_PUBLIC_ENVIRONMENT === "testnet") {
+    const api = new AxelarAssetTransfer({ environment: Environment.TESTNET });
+    depositAddress = await api.getDepositAddress(
+      srcChain.name,
+      destChain.name,
+      recipientAddress,
+      "uausdc"
+    );
+  } else {
+    depositAddress = (await getDepositAddressLocal(
+      srcChain.name,
+      destChain.name,
+      recipientAddress,
+      "aUSDC",
+      8500
+    )) as string;
+  }
 
-    // Get token address from the gateway contract for the src chain
-    const srcTokenAddress = await srcGatewayContract.tokenAddresses('aUSDC');
-    const srcErc20 = IERC20.connect(srcTokenAddress, ethereumConnectedWallet);
+  // Get token address from the gateway contract for the src chain
+  const srcTokenAddress = await srcGatewayContract.tokenAddresses("aUSDC");
+  const srcErc20 = IERC20.connect(srcTokenAddress, srcConnectedWallet);
 
-    // Get token address from the gateway contract for the destination chain
-    const destinationTokenAddress = await destGatewayContract.tokenAddresses('aUSDC');
-    const destERC20 = IERC20.connect(destinationTokenAddress, avalancheConnectedWallet);
+  // Get token address from the gateway contract for the destination chain
+  const destinationTokenAddress = await destGatewayContract.tokenAddresses(
+    "aUSDC"
+  );
+  const destERC20 = IERC20.connect(
+    destinationTokenAddress,
+    destConnectedWallet
+  );
 
-    const destBalance = await destERC20.balanceOf(recipientAddress);
+  const destBalance = await destERC20.balanceOf(recipientAddress);
 
-    const transferFee: number = await getTransferFee('ethereum', 'avalanche', 'aUSDC', amount);
+  const transferFee: number = await getTransferFee(
+    srcChain.name,
+    destChain.name,
+    "aUSDC",
+    amount
+  );
 
-    // Send the token
-    const txHash = await srcErc20
-        .transfer(depositAddress, ethers.utils.parseUnits(amount, 6))
-        .then((tx: any) => tx.wait())
-        .then((receipt: any) => receipt.transactionHash);
+  // Send the token
+  const txHash = await srcErc20
+    .transfer(depositAddress, ethers.utils.parseUnits(amount, 6))
+    .then((tx: any) => tx.wait())
+    .then((receipt: any) => receipt.transactionHash);
 
-    onSent({ txHash, depositAddress, transferFee });
+  onSent({ txHash, depositAddress, transferFee });
 
-    // Wait destination contract to execute the transaction.
-    while (true) {
-      const newBalance = await destERC20.balanceOf(recipientAddress);
-      if (!destBalance.eq(newBalance)) break;
-      await sleep(2000);
-    }
+  // Wait destination contract to execute the transaction.
+  while (true) {
+    const newBalance = await destERC20.balanceOf(recipientAddress);
+    if (!destBalance.eq(newBalance)) break;
+    await sleep(2000);
+  }
 }
--- examples-web/src/pages/examples/send-tokens/utils/index.ts
+++ examples-web/src/pages/examples/send-tokens/utils/index.ts
@@ -1,27 +1,11 @@
 import { Contract, ethers, getDefaultProvider, providers } from 'ethers';
 import {IERC20__factory as IERC20, IAxelarGateway__factory as IAxelarGateway} from 'types/contracts/factories/@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces'
-import { isTestnet, wallet } from 'config/constants';
+import { srcChain, srcConnectedWallet, destChain, destConnectedWallet } from 'config/constants';
 import {depositAddressSendToken} from './depositAddressSendToken';
 import {gatewaySendToken} from './gatewaySendToken';
 
-const chains = isTestnet
-  ? require("../../../../../config/testnet.json")
-  : require("../../../../../config/chains.json");
-
-  const ethereum = chains.find((chain: any) => chain.name === 'Ethereum') as any;
-  const avalanche = chains.find((chain: any) => chain.name === 'Avalanche') as any;
-
-  if (!ethereum || !avalanche) process.exit(0);
-
-  const useMetamask = false; // typeof window === 'object';
-
-  const ethereumProvider = useMetamask ? new providers.Web3Provider((window as any).ethereum) : getDefaultProvider(ethereum.rpc);
-  const ethereumConnectedWallet = useMetamask ? (ethereumProvider as providers.Web3Provider).getSigner() : wallet.connect(ethereumProvider);
-  const avalancheProvider = getDefaultProvider(avalanche.rpc);
-  const avalancheConnectedWallet = wallet.connect(avalancheProvider);
-
-  const srcGatewayContract = IAxelarGateway.connect(ethereum.gateway, ethereumConnectedWallet);
-  const destGatewayContract = IAxelarGateway.connect(avalanche.gateway, avalancheConnectedWallet);
+const srcGatewayContract = IAxelarGateway.connect(srcChain.gateway, srcConnectedWallet);
+const destGatewayContract = IAxelarGateway.connect(destChain.gateway, destConnectedWallet);
 
 export function truncatedAddress(address: string): string {
     return address.substring(0, 6) + '...' + address.substring(address.length - 4);
@@ -29,7 +13,7 @@ export function truncatedAddress(address: string): string {
 
 export async function getBalance(addresses: string[], isSource: boolean) {
     const contract = isSource ? srcGatewayContract : destGatewayContract;
-    const connectedWallet = isSource ? ethereumConnectedWallet : avalancheConnectedWallet;
+    const connectedWallet = isSource ? srcConnectedWallet : destConnectedWallet;
     const tokenAddress = await contract.tokenAddresses('aUSDC');
     const erc20 = new Contract(tokenAddress, IERC20.abi, connectedWallet);
     const balances = await Promise.all(
@@ -39,6 +23,14 @@ export async function getBalance(addresses: string[], isSource: boolean) {
         }),
     );
     return balances;
+}
+
+export function sleep(ms: number) {
+  return new Promise((resolve) => {
+      setTimeout(() => {
+          resolve(null);
+      }, ms);
+  });
 }
 
 export { depositAddressSendToken, gatewaySendToken}
