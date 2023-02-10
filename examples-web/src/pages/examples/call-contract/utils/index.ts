d555c34924c7737d14e2edb4d04b205c8e1e8093
npty <nptytn2@gmail.com>
2023-02-10 21:47:27 +0700

    chore: refactor call-contract

--- examples-web/src/pages/examples/call-contract/utils/index.ts
+++ examples-web/src/pages/examples/call-contract/utils/index.ts
@@ -1,30 +1,31 @@
-import { getDefaultProvider, Wallet } from "ethers";
+import {
+  srcChain,
+  destChain,
+  srcConnectedWallet,
+  destConnectedWallet,
+} from "config/constants";
+import {
+  MessageSender__factory as MessageSenderFactory,
+  MessageReceiver__factory as MessageReceiverFactory,
+} from "types/contracts/factories/contracts/call-contract";
 
-import chains from "../../../../../public/chains.json"
-import {MessageSender__factory as MessageSenderFactory, MessageReceiver__factory as MessageReceiverFactory  } from 'types/contracts/factories/contracts/call-contract'
-
-const ethereumChain = chains.find((chain: any) => chain.name === "Ethereum") || chains[0] as any;
-const avalancheChain = chains.find((chain: any) => chain.name === "Avalanche") || chains [1] as any;
-
-const mnemonic = process.env.NEXT_PUBLIC_EVM_MNEMONIC as string;
-const wallet = Wallet.fromMnemonic(mnemonic);
-
-const ethProvider = getDefaultProvider(ethereumChain.rpc);
-const ethConnectedWallet = wallet.connect(ethProvider);
-const sourceContract = MessageSenderFactory.connect(ethereumChain?.callContract, ethConnectedWallet)
-
-const avalancheProvider = getDefaultProvider(avalancheChain.rpc);
-const avalancheConnectedWallet = wallet.connect(avalancheProvider);
-const destContract = MessageReceiverFactory.connect(avalancheChain.callContract, avalancheConnectedWallet)
+const sourceContract = MessageSenderFactory.connect(
+  srcChain?.callContract,
+  srcConnectedWallet,
+);
+const destContract = MessageReceiverFactory.connect(
+  destChain.callContract,
+  destConnectedWallet,
+);
 
 export async function sendMessageToAvalanche(message: string) {
   const tx = await sourceContract.sendMessage(
-    "Avalanche",
-    avalancheChain.callContract,
+    destChain.name,
+    destChain.callContract,
     message,
     {
       value: BigInt(3000000),
-    }
+    },
   );
   await tx.wait();
 
