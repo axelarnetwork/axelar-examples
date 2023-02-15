1f1a458ada7f7733246fe2e8c01621e33bd3ea99
npty <nptytn2@gmail.com>
2023-02-15 10:49:51 +0700

    chore: update gasService address

--- examples/evm/call-contract-with-token-express/index.js
+++ examples/evm/call-contract-with-token-express/index.js
@@ -5,7 +5,7 @@ const {
     Contract,
     constants: { AddressZero },
     ethers,
-    utils,
+    utils: { keccak256, defaultAbiCoder },
     ContractFactory,
 } = require('ethers');
 const { deployUpgradable } = require('@axelar-network/axelar-gmp-sdk-solidity');
@@ -42,24 +42,35 @@ async function deployOld(chain, wallet) {
     console.log(`Deployed Registry for ${chain.name} at ${await chain.proxy.registry()}.`);
 }
 
+const getSaltFromKey = (key) => {
+    return keccak256(defaultAbiCoder.encode(['string'], [key.toString()]));
+};
+
 async function deploy(chain, wallet) {
-    console.log(`Deploying DistributionExecutable for ${chain.name}.`);
     const provider = getDefaultProvider(chain.rpc);
     chain.wallet = wallet.connect(provider);
-    console.log(`Deploying DistributionProxy for ${chain.name}.`);
     const gmpExpressServiceDeployedAddress = chain.GMPExpressService.address;
     const gmpExpressService = new Contract(gmpExpressServiceDeployedAddress, IGMPExpressService.abi, chain.wallet);
-    const salt = utils.keccak256('0x04');
+    const salt = getSaltFromKey(Date.now());
     const factory = new ContractFactory(DistributionExecutable.abi, DistributionExecutable.bytecode);
     const args = [chain.gateway, chain.gasService];
     const bytecode = factory.getDeployTransaction(...args).data;
     const owner = chain.wallet.address;
     const setupParams = '0x';
+    console.log(`Deploying DistributionExecutable for ${chain.name}.`);
     const tx = await gmpExpressService.deployExpressExecutable(salt, bytecode, owner, setupParams);
     await tx.wait(1);
+    console.log(`Deploying DistributionProxy for ${chain.name}.`);
     const deployedAddress = await gmpExpressService.deployedProxyAddress(salt, owner);
     console.log('Deployed proxy for DistributionExecutable at', deployedAddress);
-    chain.contract = new Contract(deployedAddress, DistributionExecutable.abi, chain.wallet);
+    chain.contract = new Contract(
+        deployedAddress,
+        [
+            ...DistributionExecutable.abi,
+            'function expressExecuteWithToken(string sourceChain,address sourceAddress,bytes payload,string symbol,uint256 amount)',
+        ],
+        chain.wallet,
+    );
 }
 
 async function execute(chains, wallet, options) {
@@ -67,6 +78,7 @@ async function execute(chains, wallet, options) {
     const getGasPrice = options.getGasPrice;
     const source = chains.find((chain) => chain.name === (args[0] || 'Avalanche'));
     const destination = chains.find((chain) => chain.name === (args[1] || 'Fantom'));
+
     const amount = Math.floor(parseFloat(args[2])) * 1e6 || 10e6;
     const accounts = args.slice(3);
 
@@ -88,6 +100,7 @@ async function execute(chains, wallet, options) {
 
     const approveTx = await source.usdc.approve(source.contract.address, amount);
     await approveTx.wait();
+    console.log('Approved aUSDC on', source.name);
 
     const sendTx = await source.contract.sendToMany(destination.name, destination.contract.address, accounts, 'aUSDC', amount, {
         value: BigInt(Math.floor(gasLimit * gasPrice)),
@@ -95,12 +108,13 @@ async function execute(chains, wallet, options) {
     await sendTx.wait();
     console.log('Sent tokens to distribution contract.', sendTx.hash);
 
-    // express call
+    // // express call
     const payload = ethers.utils.defaultAbiCoder.encode(['address[]'], [accounts]);
 
     const approveDestTx = await destination.usdc.approve(destination.contract.address, amount);
     await approveDestTx.wait();
-    await destination.proxy.expressExecuteWithToken(source.name, source.contract.address, payload, 'aUSDC', amount);
+    console.log('expressExecuteWithToken...');
+    await destination.contract.expressExecuteWithToken(source.name, source.contract.address, payload, 'aUSDC', amount);
 
     console.log('--- After ---');
     await logAccountBalances();
