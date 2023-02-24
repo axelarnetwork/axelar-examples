244e9d4bd0d2d72792421e9c326b85859806d34d
npty <nptytn2@gmail.com>
2023-02-24 19:30:25 +0700

    chore: refactor amount token-linker

--- examples/aptos/token-linker/index.js
+++ examples/aptos/token-linker/index.js
@@ -8,6 +8,7 @@ const {
 
 const TokenLinker = rootRequire('./artifacts/examples/aptos/token-linker/contracts/AptosTokenLinker.sol/AptosTokenLinker.json');
 const Token = require('@axelar-network/axelar-gmp-sdk-solidity/artifacts/contracts/test/ERC20MintableBurnable.sol/ERC20MintableBurnable.json');
+const { ethers } = require('ethers');
 
 const aptosTokenLinkerAddress = process.env.APTOS_TOKEN_LINKER_ADDRESS;
 const ignoreDigits = 5;
@@ -39,13 +40,19 @@ async function execute(evmChain, wallet, options) {
     const args = options.args || [];
     const client = new AptosNetwork(process.env.APTOS_URL);
     const coins = new CoinClient(client);
-    const amount1 = args[1] || BigInt(1e18);
-    const amount2 = args[2] || BigInt(Math.floor(5e17 / 256 ** ignoreDigits));
+    const amountEvmToAptos = args[1] || ethers.utils.parseEther('1');
+    const amountAptosToEvm =
+        args[2] ||
+        ethers.utils
+            .parseEther('0.5')
+            .div(256 ** ignoreDigits)
+            .toString();
 
     async function logBalances() {
-        console.log(`Balance at ${evmChain.name} is ${(await evmChain.token.balanceOf(wallet.address)) / 1e18} ALT`);
-        const balance = await coins.checkBalance(client.owner, { coinType: `${aptosTokenLinkerAddress}::token_linker::Token` });
-        console.log(`Balance at aptos is ${(Number(balance) * 256 ** ignoreDigits) / 1e18} ALT`);
+        const evmBalance = await evmChain.token.balanceOf(wallet.address).then((balance) => ethers.utils.formatEther(balance));
+        console.log(`Balance at ${evmChain.name} is ${evmBalance} ALT`);
+        const aptosBalance = await coins.checkBalance(client.owner, { coinType: `${aptosTokenLinkerAddress}::token_linker::Token` });
+        console.log(`Balance at aptos is ${(Number(aptosBalance) * 256 ** ignoreDigits) / 1e18} ALT`);
     }
 
     console.log('Initializing token storage.');
@@ -70,17 +77,17 @@ async function execute(evmChain, wallet, options) {
     const gasLimit = 3e5;
     const gasPrice = 1;
 
-    console.log(`Minting and Approving ${Number(amount1) / 1e18} ALT`);
+    console.log(`Minting and Approving ${ethers.utils.formatEther(amountEvmToAptos)} ALT`);
 
-    await (await evmChain.token.mint(wallet.address, amount1)).wait();
-    await (await evmChain.token.approve(evmChain.contract.address, amount1)).wait();
+    await (await evmChain.token.mint(wallet.address, amountEvmToAptos)).wait();
+    await (await evmChain.token.approve(evmChain.contract.address, amountEvmToAptos)).wait();
 
     console.log('--- After Mint and Approve ---');
     await logBalances();
 
     console.log(`Sending token from ${evmChain.name} to aptos`);
 
-    const tx = await evmChain.contract.sendToken(process.env.APTOS_ADDRESS, amount1, {
+    const tx = await evmChain.contract.sendToken(process.env.APTOS_ADDRESS, amountEvmToAptos, {
         value: BigInt(Math.floor(gasLimit * gasPrice)),
     });
     await tx.wait();
@@ -94,7 +101,7 @@ async function execute(evmChain, wallet, options) {
     await client.submitTransactionAndWait(client.owner.address(), {
         function: `${aptosTokenLinkerAddress}::token_linker::send_token`,
         type_arguments: [],
-        arguments: [new HexString(wallet.address).toUint8Array(), amount2, gasLimit * gasPrice],
+        arguments: [new HexString(wallet.address).toUint8Array(), amountAptosToEvm, gasLimit * gasPrice],
     });
 
     await sleep(3000);
