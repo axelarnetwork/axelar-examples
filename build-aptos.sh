64dbb53f479abab99f2fef82b184e3f1326d89fb
npty <nptytn2@gmail.com>
2023-02-26 15:28:57 +0700

    chore: fix fail aptos module compilation

--- build-aptos.sh
+++ build-aptos.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 
-aptos move compile --save-metadata --package-dir examples/aptos/call-contract/modules
-aptos move compile --save-metadata --package-dir examples/aptos/token-linker/modules
+aptos move compile --save-metadata --bytecode-version 6 --package-dir examples/aptos/call-contract/modules
+aptos move compile --save-metadata --bytecode-version 6 --package-dir examples/aptos/token-linker/modules
