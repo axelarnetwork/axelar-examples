4a6c9f53577626b4629ddb0fc0d76cd5da1a3ed5
npty <nptytn2@gmail.com>
2023-04-02 07:54:19 +0700

    chore: improve setup script

--- examples-web/scripts/setup.ts
+++ examples-web/scripts/setup.ts
@@ -1,19 +1,28 @@
 import "dotenv/config";
-import fs from "fs/promises";
+import fs from "fs";
 import path from 'path'
 
-async function main() {
-  const rootEnvPath= path.resolve(__dirname, '../../.env')
-  const rootEnv = await fs.readFile(rootEnvPath, 'utf8');
+function getEnvValue(rootEnv: any, key: string) {
   const rootEnvLines = rootEnv.split('\n');
-  const evmPrivateKey = rootEnvLines.find((line) => line.startsWith('EVM_PRIVATE_KEY'))?.split('=')[1];
-  const evmMnemonic = rootEnvLines.find((line) => line.startsWith('EVM_MNEMONIC'))?.split('=')[1];
+  return rootEnvLines.find((line: any) => line.startsWith(key))?.split('=')[1];
+}
 
-  if(!evmPrivateKey || !evmMnemonic) throw new Error('EVM_PRIVATE_KEY or EVM_MNEMONIC not found in root .env file')
+async function main() {
+  if (!fs.existsSync('.env')) {
+  const rootEnvPath= path.resolve(__dirname, '../../.env')
+  const rootEnv = fs.readFileSync(rootEnvPath);
+  const evmPrivateKey = getEnvValue(rootEnv, "EVM_PRIVATE_KEY") || ""
+  const evmMnemonic = getEnvValue(rootEnv, 'EVM_MNEMONIC') || ""
+
+  if(!evmPrivateKey && !evmMnemonic) throw new Error('EVM_PRIVATE_KEY and EVM_MNEMONIC not found in root .env file')
 
   const examplesWebEnvPath = path.resolve(__dirname, '../.env')
   const examplesWebEnv = `NEXT_PUBLIC_EVM_PRIVATE_KEY=${evmPrivateKey}\nNEXT_PUBLIC_EVM_MNEMONIC=${evmMnemonic}`;
-  await fs.writeFile(examplesWebEnvPath, examplesWebEnv);
+  await fs.writeFileSync(examplesWebEnvPath, examplesWebEnv);
+  console.log(`The setup is done! The environment variables have been written to the examples-web/.env file. You're now ready to deploy and start the application.`);
+  } else {
+    console.log(`A .env file already exist in ${path.dirname(__dirname)}, not modifying it.`);
+  }
 }
 
 main();
