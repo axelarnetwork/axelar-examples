4ff8dc879b2005a3207dffeb6b297fb48d73d570
npty <nptytn2@gmail.com>
2023-04-24 19:53:41 +0700

    chore: add config file to disable aptos by default

--- scripts/libs/start.js
+++ scripts/libs/start.js
@@ -1,5 +1,6 @@
 const { ethers } = require('ethers');
 const { createAndExport, createAptosNetwork } = require('@axelar-network/axelar-local-dev');
+const { enabledAptos: shouldRunAptos } = require('./config');
 const path = require('path');
 
 /**
@@ -9,7 +10,9 @@ const path = require('path');
  * @param {*} chains - chains to start. All chains are started if not specified (Avalanche, Moonbeam, Polygon, Fantom, Ethereum).
  */
 async function start(fundAddresses = [], chains = [], options = {}) {
-    await initAptos();
+    if (shouldRunAptos()) {
+        await initAptos();
+    }
 
     const pathname = path.resolve(__dirname, '../..', 'chain-config', 'local.json');
 
