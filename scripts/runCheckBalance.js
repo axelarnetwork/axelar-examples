45eec435f140966795c31aadb45d4f00a6b4258a
arshan <arshankhanifar@gmail.com>
2023-06-28 13:56:52 -0400

    generic check balancing

--- scripts/runCheckBalance.js
+++ scripts/runCheckBalance.js
@@ -1,54 +1,74 @@
 'use strict';
 
 const { ethers } = require('ethers');
-const { getWallet, getBalances, getEVMChains, checkEnv } = require('./libs');
+const { getBalances, getEVMChains } = require('./libs');
 const testnetChains = require('@axelar-network/axelar-cgp-solidity/info/testnet.json');
+
 const {
     contracts: { BurnableMintableCappedERC20, AxelarGateway },
 } = require('@axelar-network/axelar-local-dev');
 
-// Get the environment from the command line. If it is not provided, use 'testnet'.
-const env = process.argv[2] || 'testnet';
+const main = async () => {
+    const argv = require('yargs')
+        .usage('Usage: yarn run-script myScript --addresses=address1,address2,... [--env=<environment>]')
+        .option('env', {
+            describe: 'Environment (local or testnet)',
+            default: 'local',
+            choices: ['local', 'testnet'],
+        })
+        .demandOption(['addresses']).argv;
 
-// Check the environment. If it is not valid, exit.
-checkEnv(env);
+    const addresses = argv.addresses;
+    const env = argv.env;
 
-// Get the chains for the environment.
-const allTestnetChains = testnetChains.map((chain) => chain.name);
-const chains = getEVMChains(env, allTestnetChains);
+    // Process the addresses
+    if (!addresses) {
+        console.log('No addresses provided.');
+        process.exit(1);
+    }
 
-// Get the wallet.
-const wallet = getWallet();
+    const addressList = addresses.split(',');
 
-// Print the balances. This will print the balances of the wallet on each chain.
-console.log(`==== Print balances for ${env} =====`);
-console.log('Wallet address:', wallet.address, '\n');
+    const allTestnetChains = testnetChains.map((chain) => chain.name);
+    const chains = getEVMChains(env, allTestnetChains);
 
-async function print() {
-    console.log('Native tokens:');
-    await getBalances(chains, wallet.address).then((balances) => {
-        for (let i = 0; i < chains.length; i++) {
-            console.log(`${chains[i].name}: ${ethers.utils.formatEther(balances[chains[i].name])} ${chains[i].tokenSymbol}`);
-        }
-    });
+    async function printBalances(address) {
+        // Print the balances. This will print the balances of the wallet on each chain.
+        console.log(`==== Print balances for ${env} =====`);
+        console.log('Wallet address:', address, '\n');
 
-    console.log('\naUSDC tokens:');
+        console.log('Native tokens:');
+        await getBalances(chains, address).then((balances) => {
+            for (let i = 0; i < chains.length; i++) {
+                console.log(`${chains[i].name}: ${ethers.utils.formatEther(balances[chains[i].name])} ${chains[i].tokenSymbol}`);
+            }
+        });
 
-    const pendingBalances = chains.map((chain) => {
-        const provider = new ethers.providers.JsonRpcProvider(chain.rpc);
-        const gateway = new ethers.Contract(chain.gateway, AxelarGateway.abi, provider);
-        return gateway
-            .tokenAddresses('aUSDC')
-            .then((usdc) => {
-                const erc20 = new ethers.Contract(usdc, BurnableMintableCappedERC20.abi, provider);
-                return erc20.balanceOf(wallet.address).catch(() => '0');
-            })
-            .then((balance) => {
-                console.log(`${chain.name}: ${ethers.utils.formatUnits(balance, 6)} aUSDC`);
-            });
-    });
+        console.log('\naUSDC tokens:');
 
-    await Promise.all(pendingBalances);
-}
+        const pendingBalances = chains.map((chain) => {
+            const provider = new ethers.providers.JsonRpcProvider(chain.rpc);
+            const gateway = new ethers.Contract(chain.gateway, AxelarGateway.abi, provider);
+            return gateway
+                .tokenAddresses('aUSDC')
+                .then((usdc) => {
+                    const erc20 = new ethers.Contract(usdc, BurnableMintableCappedERC20.abi, provider);
+                    return erc20.balanceOf(address).catch(() => '0');
+                })
+                .then((balance) => {
+                    console.log(`${chain.name}: ${ethers.utils.formatUnits(balance, 6)} aUSDC`);
+                });
+        });
 
-print();
+        await Promise.all(pendingBalances);
+    }
+
+    for (let address of addressList) {
+        console.log('address', address);
+        await printBalances(address);
+    }
+};
+
+main().catch((error) => {
+    console.error('An error occurred:', error);
+});
