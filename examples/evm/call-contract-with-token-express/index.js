0c941b148fd6ee30cfefc96a5fd3ab2aa586eff2
npty <nptytn2@gmail.com>
2023-08-14 16:41:40 +0700

    chore: add some comments

--- examples/evm/call-contract-with-token-express/index.js
+++ examples/evm/call-contract-with-token-express/index.js
@@ -22,13 +22,19 @@ async function execute(_chains, wallet, options) {
     const args = options.args || [];
     const { source, destination, calculateBridgeFee, env } = options;
 
+    // Calculate the fee for the bridge.
     const fee = await calculateBridgeFee(source, destination);
+
+    // Get the amount to send.
     const amount = Math.floor(parseFloat(args[2])) * 1e6 || 10e6;
 
+    // Get the accounts to send to.
     const accounts = args.slice(3);
 
+    // If no accounts are specified, send to the default wallet address.
     if (accounts.length === 0) accounts.push(wallet.address);
 
+    // Get the balance of the first account.
     const initialBalance = await destination.usdc.balanceOf(accounts[0]);
 
     async function logAccountBalances() {
@@ -40,44 +46,36 @@ async function execute(_chains, wallet, options) {
     const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
     console.log('--- Initially ---');
+
+    // Log the balances of the accounts.
     await logAccountBalances();
 
+
+    // Approve the distribution contract to spend aUSDC.
     const approveTx = await source.usdc.approve(source.contract.address, amount);
     await approveTx.wait();
     console.log('Approved aUSDC on', source.name);
 
+    // Send tokens to the distribution contract.
     const sendTx = await source.contract
         .sendToMany(destination.name, destination.contract.address, accounts, 'aUSDC', amount, {
             value: fee,
         })
         .then((tx) => tx.wait());
+
     console.log('Sent tokens to distribution contract:', sendTx.transactionHash);
 
     if (env === 'testnet') {
         console.log(`You can track the GMP transaction status on https://testnet.axelarscan.io/gmp/${sendTx.transactionHash}\n`);
     }
 
-    // const recoveryAPI = new AxelarGMPRecoveryAPI({ environment: Environment.TESTNET });
-    // console.log(source.gateway.address, sendTx.transactionHash);
-    // let eventIndex = -1;
-
-    // for (let i = 0; i < sendTx.events.length; i++) {
-    //     if (sendTx.events[i].address === source.gateway.address) {
-    //         eventIndex = i;
-    //     }
-    // }
-
-    // const commandId = recoveryAPI.getCidFromSrcTxHash(destination.name, sendTx.transactionHash, eventIndex);
-    // const payload = ethers.utils.defaultAbiCoder.encode(['address[]'], [accounts]);
-
-    // await destination.contract.expressExecuteWithToken('0x' + commandId, source.name, source.contract.address, payload, 'aUSDC', amount);
-
-    // In this example, you won't notice much different on local because everything is fast there, try testnet env!.
+    // Wait for the distribution to complete by checking the balance of the first account.
     while ((await destination.usdc.balanceOf(accounts[0])).eq(initialBalance)) {
         await sleep(1000);
     }
 
     console.log('--- After ---');
+    // Log the balances of the accounts.
     await logAccountBalances();
 }
 
