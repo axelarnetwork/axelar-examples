42e71e3769c037469cfeed06311cfb0c96253529
npty <nptytn2@gmail.com>
2024-01-24 21:46:26 +0700

    feat: improve call-contract-with-token web example

--- examples-web/src/pages/examples/call-contract-with-token.tsx
+++ examples-web/src/pages/examples/call-contract-with-token.tsx
@@ -7,16 +7,21 @@ import {
   getBalance,
   generateRecipientAddress,
   truncatedAddress,
+  waitDestExecution,
 } from "helpers";
 
 const CallContractWithToken: NextPage = () => {
-  const [customRecipientAddress, setCustomRecipientAddress] =
-    useState<string>("");
-  const [recipientAddresses, setRecipientAddresses] = useState<string[]>([]);
-  const [balances, setBalances] = useState<string[]>([]);
+  const [destRecipientAddresses, setDestRecipientAddresses] = useState<
+    string[]
+  >([]);
+  const [sourceRecipientAddresses, setSourceRecipientAddresses] = useState<
+    string[]
+  >([]);
+  const [recipientBalances, setRecipientBalances] = useState<string[]>([]);
   const [senderBalance, setSenderBalance] = useState<string>();
   const [txhash, setTxhash] = useState<string>();
   const [loading, setLoading] = useState(false);
+  const [loadingDestBalance, setLoadingDestBalance] = useState(false);
 
   async function handleOnSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -24,39 +29,61 @@ const CallContractWithToken: NextPage = () => {
     const formData = new FormData(e.currentTarget);
     const amount = formData.get("amount") as string;
     setLoading(true);
-    await sendTokenToDestChain(amount, recipientAddresses, setTxhash).finally(
-      () => {
-        setLoading(false);
-        handleRefreshSrcBalances();
-        handleRefreshDestBalances();
-      },
-    );
-  }
+    await sendTokenToDestChain(amount, sourceRecipientAddresses, setTxhash);
 
-  const handleRefreshDestBalances = useCallback(async () => {
-    const _balances = await getBalance(recipientAddresses, false);
-    setBalances(_balances);
-  }, [recipientAddresses]);
-
-  const handleRefreshSrcBalances = useCallback(async () => {
-    console.log(wallet.address)
     const [_balance] = await getBalance([wallet.address], true);
     setSenderBalance(_balance);
-  }, []);
+
+    setLoading(false);
+    setLoadingDestBalance(true);
+
+    // Wait until the Execute event is emitted
+    await waitDestExecution();
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    const recipients = [
+      ...new Set([...destRecipientAddresses, ...sourceRecipientAddresses]),
+    ];
+    setDestRecipientAddresses(recipients);
+
+    const _balances = await getBalance(recipients, false);
+    setRecipientBalances(_balances);
+    setLoadingDestBalance(false);
+  }
+
+  const handleRefreshSrcBalances = useCallback(async () => {}, []);
 
   const handleOnGenerateRecipientAddress = () => {
     const recipientAddress = generateRecipientAddress();
-    setRecipientAddresses([...recipientAddresses, recipientAddress]);
-  };
-
-  const handleOnAddRecepientAddress = () => {
-    setRecipientAddresses([...recipientAddresses, customRecipientAddress]);
-    setCustomRecipientAddress("");
+    setSourceRecipientAddresses([
+      ...sourceRecipientAddresses,
+      recipientAddress,
+    ]);
   };
 
   useEffect(() => {
-    handleRefreshSrcBalances();
+    async function loadingBalances() {
+      const [_balance] = await getBalance([wallet.address], true);
+      setSenderBalance(_balance);
+    }
+
+    loadingBalances();
   }, [handleRefreshSrcBalances]);
+
+  function renderRecipientBalances() {
+    if (destRecipientAddresses.length) {
+      return destRecipientAddresses.map((recipientAddress, i) => (
+        <div key={recipientAddress} className="flex justify-between">
+          <span className="font-mono">
+            {truncatedAddress(recipientAddress)}
+          </span>
+          <span className="font-bold">{recipientBalances[i] || `0.00`}</span>
+        </div>
+      ));
+    } else {
+      return <p className="text-gray-500 font-lg">Empty</p>;
+    }
+  }
 
   return (
     <div>
@@ -71,22 +98,69 @@ const CallContractWithToken: NextPage = () => {
               className="h-64 bg-center bg-no-repeat bg-cover image-full"
               style={{ backgroundImage: "url('/assets/ethereum.gif')" }}
             />
-            <div className="card-body">
+            <div className="card-body flex flex-col">
               <h2 className="card-title">Ethereum (Token Sender)</h2>
 
               <p>
-                Sender ({truncatedAddress(wallet.address)}) balance:{" "}
-                {senderBalance}
+                Sender:{" "}
+                <span className="font-mono">
+                  ({truncatedAddress(wallet.address)})
+                </span>{" "}
               </p>
+              <p>Balance: {senderBalance}</p>
 
-              <label className="label">
-                <span className="label-text">Recepients</span>
-              </label>
-              {recipientAddresses.map((recipientAddress) => (
-                <span key={recipientAddress} className="mt-1">
-                  {truncatedAddress(recipientAddress)}
-                </span>
+              {sourceRecipientAddresses.length > 0 && (
+                <label
+                  className={cn("mt-1", {
+                    "opacity-30": loading,
+                    "opacity-100": !loading,
+                  })}
+                >
+                  <span className="label-text font-bold text-lg">
+                    Recipients{" "}
+                    <span className="font-bold">
+                      ({sourceRecipientAddresses.length})
+                    </span>
+                  </span>
+                </label>
+              )}
+              {sourceRecipientAddresses.map((recipientAddress) => (
+                <div className="flex items-center" key={recipientAddress}>
+                  <span className="mt-1 font-mono text-sm">
+                    {truncatedAddress(recipientAddress)}
+                  </span>
+                  <button
+                    className={cn("ml-4 text-xs", {
+                      hidden: loading,
+                      block: !loading,
+                    })}
+                    disabled={loading}
+                    onClick={() =>
+                      setSourceRecipientAddresses(
+                        sourceRecipientAddresses.filter(
+                          (addr) => addr !== recipientAddress
+                        )
+                      )
+                    }
+                  >
+                    ❌
+                  </button>
+                </div>
               ))}
+
+              <div className="form-control">
+                <button
+                  onClick={handleOnGenerateRecipientAddress}
+                  type="button"
+                  disabled={loading}
+                  className={cn("btn btn-accent mt-4", {
+                    "opacity-30": loading,
+                    "opacity-100": !loading,
+                  })}
+                >
+                  Add Random Recipient
+                </button>
+              </div>
 
               <div className="justify-end mt-2 card-actions">
                 <form
@@ -96,7 +170,7 @@ const CallContractWithToken: NextPage = () => {
                 >
                   <div>
                     <label className="label">
-                      <span className="label-text">Token amount</span>
+                      <span className="label-text">Send Amount</span>
                     </label>
                     <div className="w-full input-group">
                       <input
@@ -104,22 +178,25 @@ const CallContractWithToken: NextPage = () => {
                         required
                         name="amount"
                         type="number"
+                        min={0}
+                        step={1}
                         placeholder="Enter amount to send"
                         className="w-full input input-bordered"
                       />
-                      <button
-                        className={cn("btn btn-primary", {
-                          loading,
-                          "opacity-30":
-                            loading || recipientAddresses.length === 0,
-                          "opacity-100":
-                            !loading && recipientAddresses.length > 0,
-                        })}
-                        type="submit"
-                      >
-                        Send
-                      </button>
                     </div>
+
+                    <button
+                      className={cn("btn btn-primary mt-4 w-full", {
+                        loading,
+                        "opacity-30":
+                          loading || sourceRecipientAddresses.length === 0,
+                        "opacity-100":
+                          !loading && sourceRecipientAddresses.length > 0,
+                      })}
+                      type="submit"
+                    >
+                      Send
+                    </button>
                   </div>
                   {txhash && isTestnet && (
                     <a
@@ -130,41 +207,6 @@ const CallContractWithToken: NextPage = () => {
                       Track at axelarscan
                     </a>
                   )}
-
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text">EVM Address</span>
-                    </label>
-                    <label className="w-full input-group">
-                      <input
-                        type="text"
-                        placeholder="Enter address"
-                        className="w-full input input-bordered"
-                        value={customRecipientAddress}
-                        onChange={(e) =>
-                          setCustomRecipientAddress(e.target.value)
-                        }
-                      />
-                      <button
-                        type="button"
-                        className="btn btn-primary"
-                        onClick={handleOnAddRecepientAddress}
-                      >
-                        Add
-                      </button>
-                    </label>
-
-                    <div className="divider">OR</div>
-                    <button
-                      onClick={handleOnGenerateRecipientAddress}
-                      type="button"
-                      className={cn("btn btn-accent mt-2", {
-                        loading,
-                      })}
-                    >
-                      Generate Random Address
-                    </button>
-                  </div>
                 </form>
               </div>
             </div>
@@ -176,23 +218,24 @@ const CallContractWithToken: NextPage = () => {
               className="h-64 bg-center bg-no-repeat bg-cover image-full"
               style={{ backgroundImage: "url('/assets/avalanche.gif')" }}
             />
-            <div className="card-body">
+            <div
+              className={cn("card-body", {
+                "opacity-30": loadingDestBalance || loading,
+                "opacity-100": !loadingDestBalance && !loading,
+              })}
+            >
               <h2 className="card-title">Avalanche (Token Receiver)</h2>
               <div className="h-40">
-                <div className="w-full max-w-xs form-control">
-                  <div>
-                    {recipientAddresses.map((recipientAddress, i) => (
-                      <div
-                        key={recipientAddress}
-                        className="flex justify-between"
-                      >
-                        <span>{truncatedAddress(recipientAddress)}</span>
-                        <span className="font-bold">
-                          {balances[i] || `0.00`}
-                        </span>
-                      </div>
-                    ))}
-                  </div>
+                <div className="h-full max-w-xs form-control">
+                  {renderRecipientBalances()}
+                  {(loadingDestBalance || loading) && (
+                    <div className="flex flex-1 flex-col justify-end">
+                      <span className="font-bold">
+                        Waiting for Execution...
+                        <progress className="progress"></progress>
+                      </span>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
