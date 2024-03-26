952412f107d6b743c5f47e4a4702c517d4b5f6b5
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2024-03-26 21:28:14 +0200

    chore(amplifier): tidy up commands and update commander lib (#174)
    * chore: update commander version
    
    * chore: create separate index.js as an entrypoint for all commands
    
    * chore: export commands functions
    
    * chore: rename index.js -> amplifier.js
    
    * chore: update docs

--- examples/amplifier/endpoints/subscribe-to-wasm-events.js
+++ examples/amplifier/endpoints/subscribe-to-wasm-events.js
@@ -1,26 +1,24 @@
-const commander = require('commander');
 const newClient = require('../grpc/client');
 
-commander
-    .usage(["[OPTIONS] ..."])
-    .option("-s, --start-height <start height>", "The block height to start from (0 = latest)", parseInt, 0)
-    .parse(process.argv);
+function subscribe_to_wasm_events(startHeight) {
+    console.log("Subscribing to events starting from block:", startHeight == 0 ? "latest" : startHeight);
 
-startHeight = commander.opts().startHeight;
+    const client = newClient();
+    const call = client.SubscribeToWasmEvents({ startHeight: startHeight });
+    call.on('data', (response) => {
+        console.log("Event:", response);
+    });
+    call.on('end', () => {
+        console.log("End");
+    });
+    call.on('error', (e) => {
+        console.log("Error", e);
+    });
+    call.on('status', (status) => {
+        console.log("Status", status);
+    });
+}
 
-console.log("Subscribing to events starting from block:", startHeight == 0 ? "latest" : startHeight);
-
-const client = newClient();
-const call = client.SubscribeToWasmEvents({ startHeight: startHeight });
-call.on('data', (response) => {
-    console.log("Event:", response);
-});
-call.on('end', () => {
-    console.log("End");
-});
-call.on('error', (e) => {
-    console.log("Error", e);
-});
-call.on('status', (status) => {
-    console.log("Status", status);
-});
+module.exports = {
+    subscribe_to_wasm_events,
+};
