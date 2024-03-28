0bc459756bdd3ce2bea52a344ef1bbaa6124f4ef
ben-weinberg <weinbergben33@gmail.com>
2024-03-28 20:57:25 +0000

    [feat]: Amplifier examples (#173)
    * feat: amplifier example
    
    * feat: axelar amplifier example added
    
    * fix: added two grpc packages
    
    * chore(amplifier): tidy up commands and update commander lib (#174)
    
    * chore: update commander version
    
    * chore: create separate index.js as an entrypoint for all commands
    
    * chore: export commands functions
    
    * chore: rename index.js -> amplifier.js
    
    * chore: update docs
    
    * Update examples/amplifier/README.md
    
    * doc: added diagram for relayer flow
    
    * fix: added linux for fsevents
    
    ---------
    
    Co-authored-by: Stelios Daveas <48155711+sdaveas@users.noreply.github.com>

---
+++ examples/amplifier/endpoints/broadcast.js
@@ -0,0 +1,27 @@
+const newClient = require('../grpc/client');
+
+function broadcast(address, payload) {
+    console.log("Broadcasting message:\n", address, payload);
+
+    try {
+        JSON.parse(payload);
+    } catch (e) {
+        console.error("Payload is not valid JSON");
+        process.exit(1);
+    }
+
+    const client = newClient();
+    const broadcastRequest = { address, payload: Buffer.from(payload) };
+    response = client.Broadcast(broadcastRequest, (err, response) => {
+        if (err) {
+            console.error("Error", err);
+        } else {
+            console.log("Message sent for broadcast");
+            process.exit(0);
+        }
+    });
+}
+
+module.exports = {
+    broadcast,
+};
