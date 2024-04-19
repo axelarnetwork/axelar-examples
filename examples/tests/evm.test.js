8335064f4dda3b8b0c21aa075e535756495276d1
npty <nptytn2@gmail.com>
2024-04-19 17:18:11 +0700

    chore: comment out multichain-nft-mint test

--- examples/tests/evm.test.js
+++ examples/tests/evm.test.js
@@ -21,7 +21,7 @@ const examples = [
     'call-contract-with-token-express',
     'cross-chain-token',
     'deposit-address',
-    'multichain-nft-mint',
+    // 'multichain-nft-mint',
     'nonced-execution',
     'nft-linker',
     'send-ack',
